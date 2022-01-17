980d6c1633449ba32aed31cdb735a1d0b4666bcf
Kobong <wangkobong@gmail.com>
2022-01-17 16:53:56 +0900

    second commit

---
+++ SeSAC_Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Friends
+//
+//  Created by sungyeon kim on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
