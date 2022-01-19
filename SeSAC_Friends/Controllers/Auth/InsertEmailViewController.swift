9368a61c5ff02beee21309261c71512d8c4ceca1
wangkobong <wangkobong@gmail.com>
2022-01-19 21:25:14 +0900

    .gitignore update

---
+++ SeSAC_Friends/Controllers/Auth/InsertEmailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InsertEmailViewController.swift
+//  SeSAC_Friends
+//
+//  Created by sungyeon kim on 2022/01/19.
+//
+
+import UIKit
+
+class InsertEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
